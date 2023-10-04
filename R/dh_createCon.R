b1fe3e5978ecb9cbcb359174166134700ae36eb1
Oliver Eaton <eat_fred@protonmail.com>
2023-10-05 09:53:23 +1100

    fixed reference error in data object

--- R/dh_createCon.R
+++ R/dh_createCon.R
@@ -11,12 +11,12 @@ dh_createCon <- function(connection_string){
 
   DBI::dbConnect(
     drv = RPostgres::Postgres(),
-    user = db_info_data[connection_string]$user,
-    host = db_info_data[connection_string]$host,
-    port = db_info_data[connection_string]$port,
-    password = db_info_data[connection_string]$password,
-    dbname = db_info_data[connection_string]$database,
-    options = db_info_data[connection_string]$options
+    user = db_info_data[[connection_string]]$user,
+    host = db_info_data[[connection_string]]$host,
+    port = db_info_data[[connection_string]]$port,
+    password = db_info_data[[connection_string]]$password,
+    dbname = db_info_data[[connection_string]]$database,
+    options = db_info_data[[connection_string]]$options
   )
 
 }
